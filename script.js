67fdf3f5db16e538e14f95be0182916f9aa083e5
Jubellos <juliana.scheneider@escola.pr.gov.br>
2025-07-01 16:13:58 -0300

    ,

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ let elemento = document.querySelector ("#nome-usuario");
 elemento.textContent = nomeUsuario;
 }
 inserirNome();
+let linguagens = ["JavaScript","Pyton","C"]
+console.log(linguagens[0])
